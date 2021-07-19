7f0f6184413b7951112855a18deb4932c74e2676
Stephen Bates <sbates@raithlin.com>
2021-07-19 15:04:07 -0600

    bench: Add crc32 benchmark files for instruction counting.
    Add three simple programs that operate on variable length input memory
    files to be used in conjunction with the instruction count capability
    to assist in the benchmarking of the eBPF VM and/or JITed code.
    
    In all cases you should compile these C programs with clang and target
    ebpf as the output machine. You can then run these programs using the
    test executable and add the -i flag to perform instruction counting.

---
+++ bench/crc32.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 Eideticom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+int entry(unsigned char *input, int inp_sz) {
+
+   int i, j;
+   unsigned int byte, crc, mask;
+
+   i = 0;
+   crc = 0xFFFFFFFF;
+   while (i < inp_sz) {
+      byte = input[i];              // Get next byte.
+      crc = crc ^ byte;
+      for (j = 7; j >= 0; j--) {    // Do eight times.
+         mask = -(crc & 1);
+         crc = (crc >> 1) ^ (0xEDB88320 & mask);
+      }
+      i = i + 1;
+   }
+   return ~crc;
+}
