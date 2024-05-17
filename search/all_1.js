51686afcb4535186d9ce8415757041c4ac14a409
Github Action <ubpf@users.noreply.github.com>
2024-05-17 19:25:24 +0000

    Updated documentation
    Signed-off-by: Github Action <ubpf@users.noreply.github.com>

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['external_5ffunction_5fdispatcher_5ft_1',['external_function_dispatcher_t',['../ubpf_8h.html#aa819644ac2adf81271cf703f9eb5614b',1,'ubpf.h']]],
-  ['external_5ffunction_5ft_2',['external_function_t',['../ubpf_8h.html#a97c06ae6f43ae6e0cbca64ac1e70bc30',1,'ubpf.h']]],
-  ['external_5ffunction_5fvalidate_5ft_3',['external_function_validate_t',['../ubpf_8h.html#a924a4a1a3e782846945d6adbabbcbb9c',1,'ubpf.h']]]
+  ['basicjitmode_1',['BasicJitMode',['../ubpf_8h.html#a4be1055c33e64a9f81abf25f8b5ddedda03f63951ae56be0eb19aae34d83706f8',1,'ubpf.h']]]
 ];
