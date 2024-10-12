34087b6d46c439afe00baa506a0e97abb06a2fc7
Github Action <ubpf@users.noreply.github.com>
2024-10-12 18:42:36 +0000

    Updated documentation
    Signed-off-by: Github Action <ubpf@users.noreply.github.com>

--- search/typedefs_2.js
+++ search/typedefs_2.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['ubpf_5fbounds_5fcheck_75',['ubpf_bounds_check',['../ubpf_8h.html#a64bd2f640f675e3eeff48ea52583d699',1,'ubpf.h']]],
-  ['ubpf_5fdata_5frelocation_76',['ubpf_data_relocation',['../ubpf_8h.html#a70fb644d080e8448c1db17bb99858391',1,'ubpf.h']]],
-  ['ubpf_5fjit_5fex_5ffn_77',['ubpf_jit_ex_fn',['../ubpf_8h.html#a30c41270d593fc7ffc2161f03f8c0b5b',1,'ubpf.h']]],
-  ['ubpf_5fjit_5ffn_78',['ubpf_jit_fn',['../ubpf_8h.html#a6334115c4408267ec35087ade6128fa0',1,'ubpf.h']]]
+  ['ubpf_5fbounds_5fcheck_0',['ubpf_bounds_check',['../ubpf_8h.html#a64bd2f640f675e3eeff48ea52583d699',1,'ubpf.h']]],
+  ['ubpf_5fdata_5frelocation_1',['ubpf_data_relocation',['../ubpf_8h.html#a70fb644d080e8448c1db17bb99858391',1,'ubpf.h']]],
+  ['ubpf_5fdebug_5ffn_2',['ubpf_debug_fn',['../ubpf_8h.html#a5b194844e937155fee7888874173b0d9',1,'ubpf.h']]],
+  ['ubpf_5fjit_5fex_5ffn_3',['ubpf_jit_ex_fn',['../ubpf_8h.html#a30c41270d593fc7ffc2161f03f8c0b5b',1,'ubpf.h']]],
+  ['ubpf_5fjit_5ffn_4',['ubpf_jit_fn',['../ubpf_8h.html#a6334115c4408267ec35087ade6128fa0',1,'ubpf.h']]]
 ];
